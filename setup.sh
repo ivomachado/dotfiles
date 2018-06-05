2cc11c9ad0b8756352cfa6df88848877f93b46e1
Ivo Machado <ivo.machado@outlook.com.br>
2018-06-05 07:05:11 -0400

    `date`

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ initvimpath=~/.config/nvim/init.vim
 dotvimrcpath=~/.vimrc
 tmuxconfpath=~/.tmux.conf
 kittyconfpath=~/.config/kitty/kitty.conf
+dotscriptspath=~/.scripts
 
 rm $initvimpath
 ln -s $backupfolder/init.vim $initvimpath
@@ -18,3 +19,6 @@ ln -s $backupfolder/kitty.conf $kittyconfpath
 
 rm $tmuxconfpath
 ln -s $backupfolder/.tmux.conf $tmuxconfpath
+
+rm -r $dotscriptspath
+ln -s $backupfolder/.scripts $dotscriptspath

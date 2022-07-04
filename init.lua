d621697bd4af43e729fd9f4edeee1bbc1a48eded
Ivo Machado <ivo.machado@outlook.com.br>
2022-07-03 21:12:26 -0400

    Add support for snippets

--- init.lua
+++ init.lua
@@ -191,6 +191,12 @@ local cmp = require'cmp'
 local lspkind = require('lspkind')
 
 cmp.setup({
+    snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        end,
+    },
     mapping = {
         ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
         ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
@@ -206,10 +212,11 @@ cmp.setup({
             i = cmp.mapping.abort(),
             c = cmp.mapping.close(),
         }),
-        ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     },
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
+        { name = 'vsnip' },
         { name = 'buffer' },
         { name = 'path' },
         { name = 'nvim_lsp_signature_help' },
@@ -379,7 +386,12 @@ function inoremap(shortcut, command)
     noremap('i', shortcut, command)
 end
 
+function snoremap(shortcut, command)
+    noremap('s', shortcut, command)
+end
+
 inoremap('jk', '<Esc>')
+snoremap('jk', '<Esc>')
 
 -- Alterna a exibição do project drawer
 nmap('<leader>b', "<cmd>Neotree left toggle=true<cr>")

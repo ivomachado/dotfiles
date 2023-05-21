7f9c0e050dbd3e04bc6d4114bac1ddf2120a5313
Ivo Machado <ivo.machado@outlook.com.br>
2023-05-21 14:17:04 -0400

    Change auto-session log level to error

--- lazy_plugins.lua
+++ lazy_plugins.lua
@@ -394,7 +394,7 @@ require("lazy").setup({
         'rmagatti/auto-session',
         config = function()
             require('auto-session').setup {
-                log_level = 'info',
+                log_level = 'error',
                 auto_session_suppress_dirs = {'~/', '~/projects'}
             }
         end,

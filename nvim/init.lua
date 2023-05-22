27911664c50312f3a9babfbaea21d0ae4998e5aa
Ivo Machado <ivo.machado@outlook.com.br>
2023-05-21 22:56:02 -0400

    Futher improve config organization

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,44 +16,9 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 require("lazy").setup("plugins")
-
-vim.opt.compatible = false
-vim.opt.mouse="a"
-vim.opt.showmode = false
-vim.opt.backup = false
-vim.opt.title = true
-vim.opt.hidden = true
-vim.opt.relativenumber = true
-vim.opt.number = true
-vim.opt.swapfile = false
-vim.opt.encoding="utf-8"
-vim.opt.fileencoding="utf-8"
-vim.opt.wrap = false
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
-vim.opt.expandtab = true
-vim.opt.fixendofline = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.showtabline = 0
-
-vim.opt.termguicolors = true
-
-vim.opt.list = true
-vim.opt.listchars:append("eol:↴")
-vim.opt.listchars:append("tab:<->")
-vim.opt.listchars:append("lead:·")
-vim.opt.listchars:append("trail:·")
-vim.opt.listchars:append("extends:>")
-vim.opt.listchars:append("precedes:<")
-vim.opt.ffs="unix"
+require("opts")
 
 vim.cmd("filetype plugin on")
-
 
 vim.cmd([[
 augroup DetectIndent
@@ -179,102 +144,6 @@ local on_attach = function(client, bufnr)
     nnoremap('gr', '<cmd>Telescope lsp_references<CR>')
     nnoremap('<leader>o', '<cmd>Telescope lsp_document_symbols<CR>')
 end
-
--- Setup nvim-cmp.
--- local cmp = require'cmp'
--- local lspkind = require('lspkind')
---
--- local has_words_before = function()
---     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
---     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
--- end
---
--- local feedkey = function(key, mode)
---     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
--- end
---
--- cmp.setup({
---     snippet = {
---         expand = function(args)
---             vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
---         end,
---     },
---     mapping = {
---         ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
---         ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
---         ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
---         ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
---         ["<Tab>"] = cmp.mapping(function(fallback)
---             if cmp.visible() then
---                 cmp.select_next_item()
---             elseif vim.fn["vsnip#available"](1) == 1 then
---                 feedkey("<Plug>(vsnip-expand-or-jump)", "")
---             elseif has_words_before() then
---                 cmp.complete()
---             else
---                 fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
---             end
---         end, { "i", "s" }),
---         ["<S-Tab>"] = cmp.mapping(function()
---             if cmp.visible() then
---                 cmp.select_prev_item()
---             elseif vim.fn["vsnip#jumpable"](-1) == 1 then
---                 feedkey("<Plug>(vsnip-jump-prev)", "")
---             end
---         end, { "i", "s" }),
---         ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
---         ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
---         ['<C-e>'] = cmp.mapping({
---             i = cmp.mapping.abort(),
---             c = cmp.mapping.close(),
---         }),
---         ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
---     },
---     sources = cmp.config.sources({
---         { name = 'nvim_lsp' },
---         { name = 'nvim_lsp_signature_help' },
---         { name = 'vsnip' },
---         { name = 'buffer', keyword_length = 2 },
---         { name = 'path' },
---     }),
---     formatting = {
---         format = lspkind.cmp_format({
---             mode = 'symbol',
---             maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
---         })
---     },
---     experimental = {
---         ghost_text = false,
---     },
---     sorting = {
---         comparators = {
---             cmp.config.compare.offset,
---             cmp.config.compare.exact,
---             cmp.config.compare.recently_used,
---             require("clangd_extensions.cmp_scores"),
---             cmp.config.compare.kind,
---             cmp.config.compare.sort_text,
---             cmp.config.compare.length,
---             cmp.config.compare.order,
---         },
---     },
--- })
---
--- -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
--- cmp.setup.cmdline('/', {
---     sources = {
---         { name = 'nvim_lsp_document_symbol'},
---         { name = 'buffer'},
---     }
--- })
---
--- -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
--- cmp.setup.cmdline(':', {
---     sources = cmp.config.sources({
---         { name = 'path' },
---         { name = 'cmdline' },
---     })
--- })
 
 require("clangd_extensions").setup()
 

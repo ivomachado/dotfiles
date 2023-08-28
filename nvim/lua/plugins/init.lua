dbc323d27dd3fbfcf68ae09fc5dd9e8e28d5d93f
Ivo Machado <ivo.machado@outlook.com.br>
2023-08-28 11:09:33 -0400

    Improve Git Message Integration

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -254,10 +254,14 @@ return {
                     -- Alternatively, we can block if we find the diff-mode option
                     -- return vim.tbl_contains(argv, "-d")
                 end,
+                pre_open = function()
+                    local term = require("toggleterm.terminal")
+                    local termid = term.get_focused_id()
+                    saved_terminal = term.get(termid)
+                end,
                 post_open = function(bufnr, winnr, ft, is_blocking)
-                    if is_blocking then
-                        -- Hide the terminal while it's blocking
-                        require("toggleterm").toggle(0)
+                    if is_blocking and saved_terminal then
+                        saved_terminal:close()
                     else
                         -- If it's a normal file, just switch to its window
                         vim.api.nvim_set_current_win(winnr)
@@ -265,30 +269,31 @@ return {
 
                     -- If the file is a git commit, create one-shot autocmd to delete its buffer on write
                     -- If you just want the toggleable terminal integration, ignore this bit
-                    if ft == "gitcommit" then
+                    if ft == "gitcommit" or ft == "gitrebase" then
                         vim.api.nvim_create_autocmd(
                             "BufWritePost",
                             {
                                 buffer = bufnr,
                                 once = true,
-                                callback = function()
+                                callback = vim.schedule_wrap(function()
                                     -- This is a bit of a hack, but if you run bufdelete immediately
                                     -- the shell can occasionally freeze
-                                    vim.defer_fn(
-                                        function()
-                                            vim.api.nvim_buf_delete(bufnr, {})
-                                        end,
-                                        50
-                                    )
+                                    vim.api.nvim_buf_delete(bufnr, {})
                                 end
+                                )
                             }
                         )
                     end
                 end,
                 block_end = function()
                     -- After blocking ends (for a git commit, etc), reopen the terminal
-                    require("toggleterm").toggle(0)
-                end
+                    vim.schedule(function()
+                        if saved_terminal then
+                            saved_terminal:open()
+                            saved_terminal = nil
+                        end
+                    end)
+                end,
             },
         },
         lazy = false, priority = 1001,

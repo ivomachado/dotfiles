11824e73d4602ea9fd3a1a186ff7f0278ac043db
Ivo Machado <ivo.machado@outlook.com.br>
2023-10-29 22:18:28 -0400

    Improve tab and shift + tab behavior on autcomplete

--- nvim/lua/confs/completion.lua
+++ nvim/lua/confs/completion.lua
@@ -44,14 +44,28 @@ cmp.setup({
         ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Insert}), { 'i', 'c' }),
         ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Insert}), { 'i', 'c' }),
         ['<C-l>'] = cmp.mapping.complete({config = {sources = {{name = 'vsnip'}}}}),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if vim.fn["vsnip#jumpable"](-1) == 1 then
+                feedkey("<Plug>(vsnip-jump-prev)", "")
+            elseif cmp.visible() then
+                -- if not cmp.get_selected_entry() then
+                    cmp.select_prev_item({behavior = cmp.SelectBehavior.Insert})
+                -- end
+                -- cmp.confirm()
+            else
+                fallback()
+            end
+        end, {"i","s","c",}),
         ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                if not cmp.get_selected_entry() then
-                    cmp.select_next_item({behavior = cmp.SelectBehavior.Select})
-                end
-                cmp.confirm()
-            elseif vim.fn["vsnip#available"](1) == 1 then
-                feedkey("<Plug>(vsnip-expand-or-jump)", "")
+            if vim.fn["vsnip#jumpable"](1) == 1 then
+                feedkey("<Plug>(vsnip-jump-next)", "")
+            elseif cmp.visible() then
+                -- if not cmp.get_selected_entry() then
+                    cmp.select_next_item({behavior = cmp.SelectBehavior.Insert})
+                -- end
+                -- cmp.confirm()
+            -- elseif vim.fn["vsnip#available"](1) == 1 then
+            --     feedkey("<Plug>(vsnip-expand-or-jump)", "")
             elseif has_words_before() then
                 cmp.complete()
             else

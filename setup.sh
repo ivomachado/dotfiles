fd48a09651f2633ff362b03cb4bdb77ae1c50f67
Ivo Machado <ivo.machado@outlook.com.br>
2023-11-01 23:57:08 -0400

    Use .gitignore_global to allow better find file within helix

--- setup.sh
+++ setup.sh
@@ -9,3 +9,6 @@ zshrcpath=~/.zshrc
 ls dotconfig/ | xargs -I {} ln -T -sf $backupdir/dotconfig/{} $configdir/{}
 
 ln -sf $backupdir/.zshrc $zshrcpath
+
+git config --global core.excludesFile $PWD/.gitignore_global
+git config --global core.name "Ivo Machado"

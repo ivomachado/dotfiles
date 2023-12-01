d71e6bf4c9517a73a528b7211f6d9b8946a28825
Ivo Machado <ivo.machado@outlook.com.br>
2023-11-30 21:43:13 -0400

    Add clangd config

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 ls dotconfig/ | xargs -I {} ln -T -sf $PWD/dotconfig/{} ~/.config/{}
 
 ln -sf $PWD/.zshrc ~
+ln -sf $PWD/.clangd ~
 ln -sf $PWD/.clang-format ~
 
 git config --global core.excludesFile $PWD/.gitignore_global
